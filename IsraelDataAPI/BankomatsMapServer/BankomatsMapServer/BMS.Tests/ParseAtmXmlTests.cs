b33da093d9ed27083389e834063aa23358a947f1
constructor-igor <igor.zc@gmail.com>
2016-09-28 10:16:23 +0300

    #3 implemented loading of xml

---
+++ IsraelDataAPI/BankomatsMapServer/BankomatsMapServer/BMS.Tests/ParseAtmXmlTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Xml.Serialization;
+using NUnit.Framework;
+
+namespace BMS.Tests
+{
+    [TestFixture]
+    public class ParseAtmXmlTests
+    {
+        [Test]
+        public void XmlSerializationTest()
+        {
+            ATMs atm = new ATMs {BankomatsList = new[] {new ATM(), new ATM()}};
+
+            StringBuilder sb = new StringBuilder();
+            XmlSerializer serializer = new XmlSerializer(typeof(ATMs));
+            using (TextWriter textWriter = new StringWriter(sb))
+            {
+                serializer.Serialize(textWriter, atm);
+                Console.WriteLine(sb.ToString());
+            }
+        }
+
+        [Test]
+        public void ParseTest()
+        {
+            const string inputXmlFile = @"..\..\..\..\..\modiin-atm.xml";
+            string inputXmlPath = Path.GetFullPath(Path.Combine(AssemblyDirectory, inputXmlFile));
+            Assert.That(File.Exists(inputXmlPath), Is.True);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ATMs));
+            using (FileStream fs = new FileStream(inputXmlPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                ATMs bankomatsList = (ATMs) serializer.Deserialize(fs);
+
+                Assert.That(bankomatsList.BankomatsList.Length, Is.EqualTo(31));
+                Assert.That(bankomatsList.BankomatsList.Any(b=>b.X==0 || b.Y==0), Is.False);
+            }
+        }
+
+        private string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+    }
+
+    public class ATMs
+    {
+        [XmlElement(ElementName = "ATM")]
+        public ATM[] BankomatsList;
+    }
+
+    public class ATM
+    {
+        [XmlElement(ElementName = "קוד_בנק")]
+        public int BanksCode;
+        [XmlElement(ElementName = "קואורדינטת_X")]
+        public int X;
+        [XmlElement(ElementName = "קואורדינטת_Y")]
+        public int Y;
+    }
+}
